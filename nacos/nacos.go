85a14205b4f11b248644c9d33cf2811b6be34591
杨孙航 <13446852+yang-sunhang@user.noreply.gitee.com>
2024-05-23 19:26:43 +0800

    first commit

--- nacos/nacos.go
+++ nacos/nacos.go
@@ -2,131 +2,122 @@ package nacos
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/nacos-group/nacos-sdk-go/clients"
-	"github.com/nacos-group/nacos-sdk-go/clients/config_client"
 	"github.com/nacos-group/nacos-sdk-go/common/constant"
 	"github.com/nacos-group/nacos-sdk-go/vo"
 	"log"
 )
 
-type T struct {
+type ConfigNaCosRpc struct {
+	Port  string `json:"port" yaml:"port"`
 	Mysql struct {
-		Username  string `json:"Username"`
-		Password  string `json:"Password"`
-		Host      string `json:"Host"`
-		Port      string `json:"Port"`
-		Mysqlbase string `json:"Mysqlbase"`
-	} `json:"mysql"`
-	Grpc struct {
-		Address string `json:"Address"`
-		Host    string `json:"Host"`
-		Port    int    `json:"port"`
-	} `json:"service"`
-	Consul struct {
-		Name string `json:"name"`
-		Host string `json:"Host"`
-	} `json:"consul"`
+		Root     string `json:"root" yaml:"root"`
+		Password string `json:"password" yaml:"password"`
+		Host     string `json:"host" yaml:"host"`
+		Port     string `json:"port" yaml:"port"`
+		Database string `json:"database" yaml:"database"`
+	} `json:"mysql" yaml:"mysql"`
+	Elastic struct {
+		Host string `json:"host" yaml:"host"`
+		Port int    `json:"port" yaml:"port"`
+	} `json:"service" yaml:"elastic"`
+	JwtSigningKey string `json:"jwtSigningKey" yaml:"jwtSigningKey"`
+	Consul        struct {
+		IpAddr string `json:"IpAddr" yaml:"ipAddr"`
+	} `json:"consul" yaml:"consul"`
+	Redis struct {
+		Addr     string `json:"addr" yaml:"addr"`
+		Password string `json:"password" yaml:"password"`
+		Db       string `json:"db" yaml:"db"`
+	} `json:"redis" yaml:"redis"`
 }
 
-var GoodsT T
-var success bool
+type ConfigNaCosApi struct {
+	ServerName string `json:"serverName"`
+	Port       int    `json:"port"`
+	Jwt        struct {
+		SigningKey string `json:"signingKey"`
+	} `json:"jwt"`
+	Consul struct {
+		Host string `json:"host"`
+		Port int    `json:"port"`
+	} `json:"consul"`
+	Rabbitmq struct {
+		Root     string `json:"root"`
+		Password string `json:"password"`
+		Host     string `json:"host"`
+		Port     int    `json:"port"`
+	} `json:"rabbitmq"`
+	Crypto struct {
+		Key int64 `json:"key"`
+	} `json:"crypto"`
+	Ali struct {
+		AccessKeyID     string `json:"AccessKey_ID"`
+		AccessKeySecret string `json:"AccessKey_Secret"`
+		Endpoint        string `json:"endpoint"`
+	} `json:"ali"`
+}
 
-func createClientConfig() (constant.ClientConfig, []constant.ServerConfig) {
+func NaCos(DataId, Group, ip string, NamespaceId string) (string, error) {
 	clientConfig := constant.ClientConfig{
-		NamespaceId:         "",
+		NamespaceId:         NamespaceId,
 		TimeoutMs:           5000,
 		NotLoadCacheAtStart: true,
 		LogDir:              "/tmp/nacos/log",
 		CacheDir:            "/tmp/nacos/cache",
 		LogLevel:            "debug",
 	}
+	// At least one ServerConfig>protoc --version
 	serverConfigs := []constant.ServerConfig{
 		{
-			IpAddr:      "10.2.171.70",
+			IpAddr:      ip,
 			ContextPath: "/nacos",
 			Port:        8848,
 			Scheme:      "http",
 		},
 	}
-	fmt.Println(clientConfig, serverConfigs)
-	return clientConfig, serverConfigs
-}
-
-func NaCosConfig(Group, DataId string, Port int) {
-	clientConfig, serverConfigs := createClientConfig()
+	// Create naming service for service discovery
 	client, err := clients.CreateConfigClient(map[string]interface{}{
 		"serverConfigs": serverConfigs,
 		"clientConfig":  clientConfig,
 	})
 	if err != nil {
-		return
+		log.Println(err)
+		return "", err
 	}
-	config, err3 := client.GetConfig(vo.ConfigParam{
+	config, err := client.GetConfig(vo.ConfigParam{
 		DataId: DataId,
 		Group:  Group,
 	})
-	if err3 != nil {
-		return
-	}
-	json.Unmarshal([]byte(config), &GoodsT)
-	namingClient, err := clients.NewNamingClient(
-		vo.NacosClientParam{
-			ClientConfig:  &clientConfig,
-			ServerConfigs: serverConfigs,
-		},
-	)
-	success, err = namingClient.RegisterInstance(vo.RegisterInstanceParam{
-		Ip:          "10.2.171.70",
-		Port:        8881,
-		ServiceName: "demo.go",
-		Weight:      10,
-		Enable:      true,
-		Healthy:     true,
-		Ephemeral:   true,
-		Metadata:    map[string]string{"idc": "shanghai"},
-		ClusterName: "cluster-a", // 默认值DEFAULT
-		GroupName:   "group-a",   // 默认值DEFAULT_GROUP
-	})
-}
-
-func NaocsServiceDiscovery(Group, DataId string) {
-	clientConfig, serverConfigs := createClientConfig()
-	namingClient, err := clients.NewNamingClient(
-		vo.NacosClientParam{
-			ClientConfig:  &clientConfig,
-			ServerConfigs: serverConfigs,
-		},
-	)
-
-	// SelectAllInstance可以返回全部实例列表,包括healthy=false,enable=false,weight<=0
-	instances, err := namingClient.SelectAllInstances(vo.SelectAllInstancesParam{
-		ServiceName: "demo.go",
-		GroupName:   "group-a",             // 默认值DEFAULT_GROUP
-		Clusters:    []string{"cluster-a"}, // 默认值DEFAULT
-	})
 	if err != nil {
 		log.Println(err)
+		return "", err
+	}
+	return config, nil
+}
+
+var ServiceNac ConfigNaCosRpc
+
+func ServiceNaCos(dataid, group, host, NamespaceId string) {
+	cos, err := NaCos(dataid, group, host, NamespaceId)
+	if err != nil {
 		return
 	}
-	for i, v := range instances {
-		fmt.Println(i, v, "nacos")
+	err = json.Unmarshal([]byte(cos), &ServiceNac)
+	if err != nil {
+		return
 	}
 }
 
-func ListenConfig(client config_client.IConfigClient) {
-	//Listen config change,key=dataId+group+namespaceId.
-	err := client.ListenConfig(vo.ConfigParam{
-		DataId: "test-data",
-		Group:  "test-group",
-		OnChange: func(namespace, group, dataId, data string) {
-			fmt.Println("config changed group:" + group + ", dataId:" + dataId + ", content:" + data)
-			//dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", NaCosT.Username,
-			//	NaCosT.Password, NaCosT.Host, NaCosT.Port, NaCosT.Mysqlbase)
-			//updateDbConnection(dsn)
-		},
-	})
+var ClientNac ConfigNaCosApi
+
+func ClientNaCos(dataid, group, host, NamespaceId string) {
+	cos, err := NaCos(dataid, group, host, NamespaceId)
 	if err != nil {
-		log.Println(err)
+		return
+	}
+	err = json.Unmarshal([]byte(cos), &ClientNac)
+	if err != nil {
+		return
 	}
 }
