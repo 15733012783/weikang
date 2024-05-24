a3103730eef64b269968e8e1f3d6a9a21098781b
小瑜儿000 <yumo040410@163.com>
2024-05-24 11:46:06 +0800

    nacos动态监听

--- config/config.go
+++ config/config.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"github.com/fsnotify/fsnotify"
 	"github.com/spf13/viper"
+	"log"
 )
 
 func InitViper(fileName, filePath, configName string) (map[string]string, error) {
@@ -13,4 +15,51 @@ func InitViper(fileName, filePath, configName string) (map[string]string, error)
 	}
 	mapString := viper.GetStringMapString(configName)
 	return mapString, nil
+}
+
+type BlogBff struct {
+	//ServiceName string       `yaml:"serverName"`
+	Nacos NacosConfig `mapstructure:"nacosConfig"`
+}
+type NacosConfig struct {
+	ServerConfig struct {
+		IpAddr string `mapstructure:"IpAddr"`
+		Port   uint64 `mapstructure:"Port"`
+	}
+	ClientConfig struct {
+		NamespaceId string `mapstructure:"NamespaceId"`
+	}
+	ConfigParam struct {
+		DataId string `mapstructure:"DataId"`
+		Group  string `mapstructure:"Group"`
+	}
+}
+
+var NacosConf NacosConfig
+
+func InitViperByStruct(configFile string) error {
+	v := viper.New()
+	v.SetConfigFile(configFile)
+
+	err := v.ReadInConfig()
+	if err != nil {
+		return err
+	}
+	err = v.Unmarshal(&NacosConf)
+	if err != nil {
+		return err
+	}
+
+	v.WatchConfig()
+	//动态监听
+
+	v.OnConfigChange(func(c fsnotify.Event) {
+		err = v.Unmarshal(&NacosConf)
+		if err != nil {
+			return
+		}
+		log.Println("本地配置文件发生变动")
+	})
+	log.Println("viper初始化完成")
+	return nil
 }
