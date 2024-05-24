af9879eea79287ef3f7b8c9c1340f8464dd773b5
杨孙航 <13446852+yang-sunhang@user.noreply.gitee.com>
2024-05-24 16:45:24 +0800

    更新包

--- client/client.go
+++ client/client.go
@@ -1,17 +1,10 @@
 package client
 
 import (
-	"fmt"
-	"github.com/15733012783/mysql/consul"
 	_ "github.com/mbobakov/grpc-consul-resolver"
 	"google.golang.org/grpc"
 )
 
 func Client(serverName, Address string) (*grpc.ClientConn, error) {
-	conn, err := consul.GetClient(serverName, Address)
-	if err != nil {
-		return nil, err
-	}
-	fmt.Println(conn)
 	return grpc.Dial("consul://10.2.171.70:8500/"+serverName+"?wait=14s", grpc.WithInsecure(), grpc.WithDefaultServiceConfig(`{"LoadBalancingPolicy": "round_robin"}`))
 }
